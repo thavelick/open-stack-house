ac24ac003d088e660c22230008fdc2de8859ebe7
Tristan Havelick <tristan@havelick.com>
2025-02-22 23:17:02 -0700

    fix: Update GameState to use FlxShapeBox for border drawing

--- source/GameState.hx
+++ source/GameState.hx
@@ -13,13 +13,15 @@ class GameState extends FlxState
     override public function create()
     {
         super.create();
-        
+
         // Create border
+	# ai! FlxSprite doesn't have a drawrect. I think you want https://api.haxeflixel.com/flixel/addons/display/shapes/FlxShapeBox.html
         border = new FlxSprite(0, 0);
         border.makeGraphic(FlxG.width, FlxG.height, FlxColor.TRANSPARENT);
+
         border.drawRect(0, 0, FlxG.width, FlxG.height, FlxColor.TRANSPARENT, {color: FlxColor.BLUE, thickness: 2});
         add(border);
-        
+
         // Create player at bottom center of screen
         player = new Player((FlxG.width - 32) / 2, FlxG.height - 48);
         add(player);
