a159ef3510be914f484dd003f3e3375363154d68
Tristan Havelick <tristan@havelick.com>
2025-02-22 22:29:44 -0700

    feat: Add PlayState class definition in PlayState.hx

---
+++ source/PlayState.hx
@@ -0,0 +1,18 @@
+ou/Playackage;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+
+		# ai! Let's add a string here with "Open Stack House" See: https://haxeflixel.com/documentation/hello-world/
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
